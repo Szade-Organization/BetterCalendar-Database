521cfd661981aaaf38271c90cd3d8cb799dfd35f
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-02-03 13:34:28 +0100

    Update 01user.js

--- scripts/01user.js
+++ scripts/01user.js
@@ -1,8 +1,5 @@
 data = [
   {
-    "_id": {
-      "$oid": "6580ba36c4b5c936941a5405"
-    },
     "id": 1,
     "password": "pbkdf2_sha256$320000$VZo3Dku5xrlIxwRopyC421$wAepeKz3O0GGAHkAKGGluMmcWhd/vnIkCNMD0kxEdEc=",
     "last_login": null,
@@ -18,9 +15,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba36c4b5c936941a5406"
-    },
     "id": 2,
     "password": "pbkdf2_sha256$320000$UkisGteyhB4XZSkmdmM4pe$4muCrAx9nymo4R0F0gZWSJ8olU6sbwT0qrucBH4VRzY=",
     "last_login": null,
@@ -36,9 +30,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba36c4b5c936941a5407"
-    },
     "id": 3,
     "password": "pbkdf2_sha256$320000$i3ID4M27G7SPP7Qbw6Cs7z$hWV6H0NJRqkTaVOr23hYlxjVqos+40U9u5tDvtU1PUc=",
     "last_login": null,
@@ -54,9 +45,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba36c4b5c936941a5408"
-    },
     "id": 4,
     "password": "pbkdf2_sha256$320000$WXVtaXe7oZDid2l9pBBUYg$Ud4l1eensZ0nP3k3Kw+P7Vj2oUb32EPta7Q6I2G420Q=",
     "last_login": null,
@@ -72,9 +60,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba37c4b5c936941a5409"
-    },
     "id": 5,
     "password": "pbkdf2_sha256$320000$sZ4RhTFkJCKy1Fg07vVXtC$wGnX60svEBHmiyhylLYxRgjSdULt4MVJQB90863DnO8=",
     "last_login": null,
@@ -90,9 +75,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba37c4b5c936941a540a"
-    },
     "id": 6,
     "password": "pbkdf2_sha256$320000$LlUXqWdiruZaunNCD9Wjpu$/LmS4TztWnPjtzWI4plB/eLJp4LjTfhGVJ10cujBe9c=",
     "last_login": null,
@@ -108,9 +90,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba37c4b5c936941a540b"
-    },
     "id": 7,
     "password": "pbkdf2_sha256$320000$ccS1o4eRzd438j2vMS45As$WAjbSKF7Kf6vuIUkItqZFxYrB7xFwwCdq4U2zzNT7As=",
     "last_login": null,
@@ -126,9 +105,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba37c4b5c936941a540c"
-    },
     "id": 8,
     "password": "pbkdf2_sha256$320000$X0blMmYCKR0i078u9rbGnH$bwglIQYAbr1TMzguDmlevz6eycguDUJe7YZrP8YtxHs=",
     "last_login": null,
@@ -144,9 +120,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba37c4b5c936941a540d"
-    },
     "id": 9,
     "password": "pbkdf2_sha256$320000$jiu95wmJcenQ7fE58XhWUP$pGu8WM+cKSojvu4g5TAIbniGeH8CcoYKR5M+3aU5pB0=",
     "last_login": null,
@@ -162,9 +135,6 @@ data = [
     }
   },
   {
-    "_id": {
-      "$oid": "6580ba38c4b5c936941a540e"
-    },
     "id": 10,
     "password": "pbkdf2_sha256$320000$QhK9fBQ7gbrvZyu1OJUXdL$3K52HvL1kQeBeVhs0xc6ynvwol7dsGMTA1bieaHVwR4=",
     "last_login": null,
